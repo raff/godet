c6a7107e02e37a1decaaf0de6ec3890b3603845a
Raffaele Sena <raff367@gmail.com>
2017-05-04 21:44:03 -0700

    remove time.Sleep from example

---
+++ examples/example.go
@@ -0,0 +1,76 @@
+// +build ignore
+package main
+
+import "fmt"
+import "time"
+
+import "github.com/raff/godet"
+
+func main() {
+	// connect to Chrome instance
+	remote, _ := godet.Connect("localhost:9222", true)
+
+	// disconnect when done
+	defer remote.Close()
+
+	// get browser and protocol version
+	version, _ := remote.Version()
+	fmt.Println(version)
+
+	// get list of open tabs
+	tabs, _ := remote.TabList("")
+	fmt.Println(tabs)
+
+	// install some callbacks
+	remote.CallbackEvent(godet.EventClosed, func(params godet.Params) {
+		fmt.Println("RemoteDebugger connection terminated.")
+	})
+
+	remote.CallbackEvent("Network.requestWillBeSent", func(params godet.Params) {
+		fmt.Println("requestWillBeSent",
+			params["type"],
+			params["documentURL"],
+			params["request"].(map[string]interface{})["url"])
+	})
+
+	remote.CallbackEvent("Network.responseReceived", func(params godet.Params) {
+		fmt.Println("responseReceived",
+			params["type"],
+			params["response"].(map[string]interface{})["url"])
+	})
+
+	remote.CallbackEvent("Log.entryAdded", func(params godet.Params) {
+		entry := params["entry"].(map[string]interface{})
+		fmt.Println("LOG", entry["type"], entry["level"], entry["text"])
+	})
+
+	// block loading of most images
+	_ = remote.SetBlockedURLs("*.jpg", "*.png", "*.gif")
+
+	// create new tab
+	tab, _ := remote.NewTab("https://www.google.com")
+	fmt.Println(tab)
+
+	// enable event processing
+	remote.RuntimeEvents(true)
+	remote.NetworkEvents(true)
+	remote.PageEvents(true)
+	remote.DOMEvents(true)
+	remote.LogEvents(true)
+
+	// navigate in existing tab
+	_ = remote.ActivateTab(tabs[0])
+
+	// re-enable events when changing active tab
+	remote.AllEvents(true) // enable all events
+
+	_, _ = remote.Navigate("https://www.google.com")
+
+	// evaluate Javascript expression in existing context
+	res, _ := remote.EvaluateWrap(`
+            console.log("hello from godet!")
+            return 42;
+        `)
+
+	fmt.Println(res)
+}

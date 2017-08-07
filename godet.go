4f8293b9025402c2da5adc67c22bb4241294b2d6
Marco Lee <liarc740@gmail.com>
2017-08-07 19:27:21 +0800

    fix reading responses map without locking

--- godet.go
+++ godet.go
@@ -346,9 +346,11 @@ func (remote *RemoteDebugger) SendRequest(method string, params Params) (map[str
 
 // sendRawReplyRequest sends a request and returns the reply bytes.
 func (remote *RemoteDebugger) sendRawReplyRequest(method string, params Params) ([]byte, error) {
+	responseChann := make(chan json.RawMessage, 1)
+
 	remote.Lock()
 	reqID := remote.reqID
-	remote.responses[reqID] = make(chan json.RawMessage, 1)
+	remote.responses[reqID] = responseChann
 	remote.reqID++
 	remote.Unlock()
 
@@ -359,7 +361,7 @@ func (remote *RemoteDebugger) sendRawReplyRequest(method string, params Params) 
 	}
 
 	remote.requests <- command
-	reply := <-remote.responses[reqID]
+	reply := <-responseChann
 
 	remote.Lock()
 	delete(remote.responses, reqID)
